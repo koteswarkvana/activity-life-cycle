461269f7ade97ed5f785db5c35fc966cae228165
Koteswarao <kotesh@kvana.com>
2017-11-11 12:36:59 +0530

    register created

--- app/src/main/java/com/koti/apple/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/koti/apple/activitylifecycle/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     @Override
@@ -11,6 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e(TAG, "onCreate: >>> ");
+        Log.e(TAG, "onCreate: savedInstanceState >>> "+savedInstanceState);
+
+        new Thread(new Runnable() {
+            public void run() {
+                //code
+                try {
+                    InstanceID instanceID = InstanceID.getInstance(getApplicationContext());
+
+                    String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+                            GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+
+                    Log.i(TAG, "GCM Registration Token: " + token);
+
+                }catch (Exception e) {
+                    Log.d(TAG, "Failed to complete token refresh", e);
+                }
+            }
+        }).start();
+
     }
 
     @Override
